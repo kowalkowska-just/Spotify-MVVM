4636f60bd2fa6093a1c107b766ec09314fbde06d
Justyna <kowalkowska.just@gmail.com>
2021-02-17 21:25:03 +0100

    Creating an UIView Extension for constraints constant values: width, height, left, right, top and bottom.

---
+++ Spotify/Extension/Extension-UIView.swift
@@ -0,0 +1,35 @@
+//
+//  Extension-UIView.swift
+//  Spotify
+//
+//  Created by Justyna Kowalkowska on 17/02/2021.
+//
+
+import UIKit
+
+extension UIView {
+    
+    var width: CGFloat {
+        return frame.size.width
+    }
+    
+    var height: CGFloat {
+        return frame.size.height
+    }
+    
+    var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    var right: CGFloat {
+        return left + width
+    }
+    
+    var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        return top + height
+    }
+}
